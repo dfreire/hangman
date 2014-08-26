f4c2ef26e68dec545f9ea471d131e5523f570ebb
dfreire <dario.freire@gmail.com>
2014-08-26 21:26:20 +0100

    update

--- hangman.go
+++ hangman.go
@@ -1,7 +1,13 @@
 package hangman
 
+import (
+	"github.com/boltdb/bolt"
+)
+
 type Aggregate struct {
+	db *bolt.DB
 }
 
-func New() {
+func New(db *bolt.DB) *Aggregate {
+	return &Aggregate{db: db}
 }
--- hangman_test.go
+++ hangman_test.go
@@ -1,10 +1,28 @@
 package hangman_test
 
 import (
+	"github.com/boltdb/bolt"
 	"github.com/dfreire/hangman"
+	"os"
 	"testing"
 )
 
 func TestCreate(t *testing.T) {
-	hangman.New()
+	db := openBoltDB()
+	defer closeBoltDB(db)
+
+	hangman.New(db)
+}
+
+func openBoltDB() *bolt.DB {
+	db, err := bolt.Open("test.db", 0600, nil)
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func closeBoltDB(db *bolt.DB) {
+	defer os.Remove(db.Path())
+	db.Close()
 }
