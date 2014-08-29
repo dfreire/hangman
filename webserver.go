6bcad8f030e49bed3f8137bec8d187ff64aea85f
dfreire <dario.freire@gmail.com>
2014-08-29 22:29:37 +0100

    master

--- webserver.go
+++ webserver.go
@@ -5,20 +5,12 @@ import (
 	"github.com/martini-contrib/render"
 )
 
-type A struct {
-	B B
-}
-
-type B struct {
-	C int
-}
-
 func Run() {
 	m := martini.Classic()
 	m.Use(render.Renderer())
 
 	m.Get("/", func(r render.Render) {
-		r.HTML(200, "index", A{B: B{C: 123}})
+		r.HTML(200, "index", "", render.HTMLOptions{Layout: "_layout"})
 	})
 	m.Run()
 }
