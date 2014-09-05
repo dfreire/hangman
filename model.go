a29f44fa0b9339f193d3eb7856cc22babf40fb95
dfreire <dario.freire@gmail.com>
2014-09-05 23:48:58 +0100

    uppdate

--- model.go
+++ model.go
@@ -1,14 +1,16 @@
 package hangman
 
 type Game struct {
-	Id        string
-	Theme     string
-	Clue      string
-	Answer    string
-	Url       string
-	AuthorId  string
-	Approved  bool
-	FlagCount int
+	Id          string
+	Theme       string
+	Clue        string
+	Answer      string
+	Url         string
+	AuthorId    string
+	Approved    bool
+	FlagCount   int
+	LikeCount   int
+	UnlikeCount int
 }
 
 type Author struct {
