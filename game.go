0f677f2e6aa3a1be99efdfcc8da9b0778cb3d6d0
dfreire <dario.freire@gmail.com>
2014-09-06 21:05:13 +0100

    update

--- game.go
+++ game.go
@@ -6,6 +6,8 @@ import (
 
 const (
 	CreatedGameEvent event.Type = "CreatedGameEvent"
+	UpdatedGameEvent event.Type = "UpdatedGameEvent"
+	RemovedGameEvent event.Type = "RemovedGameEvent"
 )
 
 func (self *Hangman) CreateGame(appId, clue, answer, url, authorId string) (evt event.Event, err error) {
@@ -24,4 +26,40 @@ func onCreatedGame(evt event.Event) error {
 
 func (self *Hangman) OnCreatedGame(evt event.Event) error {
 	return onCreatedGame(evt)
+}
+
+func (self *Hangman) UpdateGame(appId, clue, answer, url, authorId string) (evt event.Event, err error) {
+	evt = event.NewEvent(UpdatedGameEvent, 1, Game{
+		Clue:     clue,
+		Answer:   answer,
+		Url:      url,
+		AuthorId: authorId,
+	})
+	return
+}
+
+func onUpdatedGame(evt event.Event) error {
+	return nil
+}
+
+func (self *Hangman) OnUpdatedGame(evt event.Event) error {
+	return onUpdatedGame(evt)
+}
+
+func (self *Hangman) RemoveGame(appId, clue, answer, url, authorId string) (evt event.Event, err error) {
+	evt = event.NewEvent(RemovedGameEvent, 1, Game{
+		Clue:     clue,
+		Answer:   answer,
+		Url:      url,
+		AuthorId: authorId,
+	})
+	return
+}
+
+func onRemovedGame(evt event.Event) error {
+	return nil
+}
+
+func (self *Hangman) OnRemovedGame(evt event.Event) error {
+	return onRemovedGame(evt)
 }
